f096e5f00d2bfd1f703daf37d39f227d47251f34
alska95 <rhsalska55@naver.com>
2021-03-21 19:50:27 +0900

    day1

--- src/Aside.js
+++ src/Aside.js
@@ -7,13 +7,15 @@ function Aside({flagInfo}){
     const checkBox4Flag = flagInfo[3];
     const imgFlag = flagInfo[4];
 
+    const disease = [["Pnumonia",82] , ["nodule",63] , ["disease" ,54] , ["disease" , 46]];
+
     return(
         <React.Fragment>
             <aside className={"aside"}>
-                {heatMapFlag === 1 && imgFlag === 1? <div><div className={"redBlock"}></div> <div>Penumonia 82%</div></div> : "" }
-                {boxFlag === 1 && imgFlag === 1? <div><div className={"blueBlock"}></div> <div>nodule 63%</div></div> : "" }
-                {checkBox3Flag === 1 && imgFlag === 1? <div><div className={"greenBlock"}></div> <div>decease 54%</div></div> : "" }
-                {checkBox4Flag === 1 && imgFlag === 1? <div><div className={"blackBlock"}></div> <div>decease 46%</div></div> : "" }
+                {heatMapFlag === 1 && imgFlag === 1? <div><div className={"redBlock"}></div> <div>{disease[0][0] + " " + disease[0][1]}%</div></div> : "" }
+                {boxFlag === 1 && imgFlag === 1? <div><div className={"blueBlock"}></div> <div>{disease[1][0] + " " + disease[1][1]}%</div></div> : "" }
+                {checkBox3Flag === 1 && imgFlag === 1? <div><div className={"greenBlock"}></div> <div>{disease[2][0] + " " + disease[2][1]}%</div></div> : "" }
+                {checkBox4Flag === 1 && imgFlag === 1? <div><div className={"blackBlock"}></div> <div>{disease[3][0] + " " + disease[3][1]}%</div></div> : "" }
             </aside>
         </React.Fragment>
     )
